6d1601e12db203681014e5742484111e1e9853a0
Ashif Hasan <hashif751@gmail.com>
2024-07-13 14:39:33 +0530

    updating models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.db.models import Avg
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from datetime import timedelta
+
 
 class Vendor(models.Model):
     name = models.CharField(max_length=50)
@@ -42,5 +47,37 @@ class Historical_Performance(models.Model):
     def __str__(self):
         return self.vendor
 
+@receiver(post_save, sender=Purchase_Order)
+def update_delivery_time(sender, instance, **kwargs):
+    if instance.status == 'pending' and instance.delivery_date is None:
+        if instance.acknowledgement_date is None:
+            instance.delivery_date= instance.issue_date + timedelta(days=10)
+        else:
+            instance.delivery_date=instance.acknowledgement_date + timedelta(days=7)
+
+        instance.save()
 
 
+@receiver(post_save, sender=Purchase_Order)
+def onTime_delivery_rate(sender, instance, **kwargs):
+    if instance.status == 'completed':
+        completed_orders = Purchase_Order.objects.filter(vendor=instance.vendor, status='completed')
+        timed_deliveries = completed_orders.filter(delivery_date__lte= instance.delivery_date) #lte=lessthanequal
+
+        ONDR = timed_deliveries.count() / completed_orders.count()
+        instance.vendor.on_time_delivery_rate = ONDR if ONDR is not None else 0.0
+    
+        instance.save()
+
+
+@receiver(post_save, sender=Purchase_Order)
+def quality_rating_average(sender, instance, **kwargs):
+    completed_PO_rating = Purchase_Order.objects.filter(vendor=instance.vendor, status='completed')
+    average_quality_rating = completed_PO_rating.aggregate(Avg('quality_rating')) ['quality_rating_avg']
+    instance.vendor.quality_rating_avg = average_quality_rating if average_quality_rating is not None else 0.0
+    instance.save()
+
+
+
+
+        
